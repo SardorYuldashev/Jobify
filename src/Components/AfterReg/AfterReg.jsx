1efb8e93195c2803abe449b98c0cf713f1327461
Sardor <henkook-baron@mail.ru>
2023-03-05 13:02:25 +0500

    Kirish-chiqish, reg, login, logOut tog'rilandi

--- src/Components/AfterReg/AfterReg.jsx
+++ src/Components/AfterReg/AfterReg.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { loadUserCompleted, loadUserEmail, loadUserID, loadUserInfo, loadUserName, loadUserToken } from '../../store/slices/user'
 import classes from './afterReg.module.scss'
 
 const AfterReg = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+
+  function logOut () {
+    let question = confirm("Siz rostdan ham chiqmoqchimisiz?")
+    if (!question) return
+    localStorage.clear()
+    dispatch(loadUserCompleted(false))
+    dispatch(loadUserEmail(null))
+    dispatch(loadUserID(null))
+    dispatch(loadUserInfo(null))
+    dispatch(loadUserName(null))
+    dispatch(loadUserToken(null))
+    navigate("/")
+  }
+
+
+
+
   return (
     <>
       <ul className={classes["header__nav-list"]}>
@@ -20,7 +42,7 @@ const AfterReg = () => {
         </li>
 
         <li className={classes["header__nav-li"]}>
-          <button className={classes["header__nav-btn"]}>Logout</button>
+          <button onClick={logOut} className={classes["header__nav-btn"]}>Logout</button>
         </li>
 
       </ul>
--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -4,47 +4,59 @@ import AfterProfile from '../../Components/AfterProfile'
 import BeforeProfile from '../../Components/BeforeProfile'
 import classes from './dashboard.module.scss'
 import axios from 'axios'
-import { loadUserEmail, loadUserID, loadUserName } from '../../store/slices/user'
+import { loadUserCompleted, loadUserEmail, loadUserID, loadUserInfo, loadUserName } from '../../store/slices/user'
 import { toast } from 'react-toastify'
 
-const Dashboard = () => {  
+const Dashboard = () => {
   const dispatch = useDispatch()
   const [udachno, setUdachno] = useState(false)
+  const [loading, setloading] = useState(false)
 
   useEffect(() => {
-    (async function getUserName () {
-      let {data} = await axios.get('/auth')      
-      if(data) {
+    (async function getUserName() {
+      let { data } = await axios.get('/auth')
+      if (data) {
         dispatch(loadUserEmail(data.email))
         dispatch(loadUserName(data.name))
         dispatch(loadUserID(data._id))
         localStorage.setItem("userEmail", data.email)
         localStorage.setItem("userName", data.name)
-        localStorage.setItem("userID", data._id)        
+        localStorage.setItem("userID", data._id)
       }
     })()
 
-    async function getFull () {
+    async function getFull() {
       try {
-        let {data} = await axios.get('/profile/me')
-        setUdachno(true)
+        setloading(true)
+        let { data } = await axios.get('/profile/me')
+        if (data) {
+          dispatch(loadUserCompleted(true))
+          localStorage.setItem("isCompleted", true)
+          localStorage.setItem("userInfo", JSON.stringify(data))
+          dispatch(loadUserInfo(data))
+          setUdachno(true)
+          setloading(false)
+        }
         
       } catch (error) {
+        dispatch(loadUserCompleted(false))
+        localStorage.setItem("isCompleted", false)
         setUdachno(false)
-        
+        setloading(false)
+
       }
     }
     getFull()
 
   }, [udachno])
 
-  let { token } = useSelector(({user}) => user)
-  let { isCompleted } = useSelector(({user}) => user)
+  let { token } = useSelector(({ user }) => user)
+  let { isCompleted } = useSelector(({ user }) => user)
 
   return (
     <div className={classes["dashboard"]}>
       <div className="container">
-        {token && udachno && isCompleted ? <AfterProfile /> : <BeforeProfile />}
+        {loading ? <h2>Loading</h2> : token && udachno && isCompleted ? <AfterProfile /> : <BeforeProfile />}
       </div>
     </div>
   )
